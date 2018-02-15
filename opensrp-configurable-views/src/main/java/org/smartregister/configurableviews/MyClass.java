192da07e625278e580577c311b26ee1b32a715c9
Martin Ndegwa <ndegwamartin@gmail.com>
2018-02-15 18:28:11 +0300

    Set up foundational project:   - layout   - structure   - testing   - C.I   - maven publishing

---
+++ opensrp-configurable-views/src/main/java/org/smartregister/configurableviews/MyClass.java
@@ -0,0 +1,11 @@
+package org.smartregister.configurableviews;
+
+/**
+ * Created by ndegwamartin on 15/02/2018.
+ */
+
+public class MyClass {
+    public int testAddition(int a, int b) {
+        return a + b;
+    }
+}
---
+++ opensrp-configurable-views/src/test/java/org/smartregister/configurableviews/BaseUnitTest.java
@@ -0,0 +1,19 @@
+package org.smartregister.configurableviews;
+
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by ndegwamartin on 15/02/2018.
+ */
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
+public abstract class BaseUnitTest {
+}
