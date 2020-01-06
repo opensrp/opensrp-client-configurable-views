c7b23bfa777b2ffbb253521e63b9038ea1088414
Samuel Githengi <samuel.githengi@gmail.com>
2020-01-06 14:16:42 +0300

    upgrade core libarr

--- opensrp-configurable-views/src/main/java/org/smartregister/configurableviews/ConfigurableViewsLibrary.java
+++ opensrp-configurable-views/src/main/java/org/smartregister/configurableviews/ConfigurableViewsLibrary.java
@@ -43,7 +43,7 @@ public class ConfigurableViewsLibrary {
 
     public ConfigurableViewsRepository getConfigurableViewsRepository() {
         if (configurableViewsRepository == null) {
-            configurableViewsRepository = new ConfigurableViewsRepository(getRepository());
+            configurableViewsRepository = new ConfigurableViewsRepository();
         }
         return configurableViewsRepository;
     }
